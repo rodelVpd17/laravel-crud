a71f52a04493babc5055fd5e847ad84aafeb6565
Rodel Agocoy <rodelagocoy.vpd@gmail.com>
2024-06-10 18:00:22 +0800

    added some functions

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -44,4 +44,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function usersCoolPosts() {
+        return $this->hasMany(Post::class, 'user_id');
+    }
 }
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Models\Post;
+use App\Http\Controllers\SignUp;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    return view('welcome');
+    $posts = [];
+
+    if(auth()->check()){
+        $posts = Post::all();
+        $posts = auth()->user()->usersCoolPosts()->latest()->get();
+    }
+
+    return view('home', ['posts' => $posts]);
 });
+
+// User related routes
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/logout', [UserController::class, 'logout']);
+Route::post('/login', [UserController::class, 'login']);
+
+// Blog post related routes
+Route::post('/create-post', [PostController::class, 'createPost']);
+Route::get('/edit-post/{post}', [PostController::class, 'showEditScreen']);
+Route::post('/edit-post/{post}', [PostController::class, 'editPost']);
